1db4ead0a42e85ab1953044064a250fad9a40843
Areej Iqbal <81074839+aiqbal15@users.noreply.github.com>
2022-04-24 21:55:37 +0500

    task_1: Initial changes

---
+++ MVC_Practice/MVC_Practice/Controllers/MoviesController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using MVC_Practice.Models;
+
+namespace MVC_Practice.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            Movie movie = new Movie() { Name = "Shrek!" };
+            return View(movie);
+        }
+    }
+}
